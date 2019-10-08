1a1fc960115d00d324841c68ab565cb3d5b85495
Simon Beaulieu <s.beaulie2@gmail.com>
2019-10-07 23:08:37 -0400

    rename parse function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { parseToNumber } from './app/parser';
+import { parseNumber } from './app/parser';
 import { formatNumber } from './app/formatter';
 
 (async () => {
   const input = process.env.INPUT_TOFORMAT || '1234.567';
 
-  const parsedInput = parseToNumber(input);
+  const parsedInput = parseNumber(input);
   const formatted = formatNumber(parsedInput);
   console.log('The money amount is %s', formatted);
 })();
